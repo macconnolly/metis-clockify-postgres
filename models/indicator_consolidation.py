d7e50d0f7ac249af43c61460fce443100a2aad9e
Mac Connolly <Mac.Connolly@metisstrategy.com>
2020-09-07 14:03:49 -0600

    Updated indicators

--- models/indicator_consolidation.py
+++ models/indicator_consolidation.py
@@ -27,7 +27,7 @@ class IndicatorConsolidation(Model):
     @staticmethod
     def calculate_prep(start, end, member, neo_id=None):
         if neo_id is None:
-            neo_id = Client.where("name", "Firm Initiative").first().id
+            neo_id = Client.where("name", "prep").first().id
         if member.date_deactivated is not None:
             if start >= member.date_deactivated:
                 return None
