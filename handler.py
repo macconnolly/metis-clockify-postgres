c902df060442068ba38fe5da9bc01fdfe58c81e6
Mac Connolly <Mac.Connolly@metisstrategy.com>
2020-09-10 11:53:05 -0600

    Added environment variables

--- handler.py
+++ handler.py
@@ -6,11 +6,13 @@ import json
 import datetime
 from models import *
 
+Member.save_from_clockify()
+
 def update(event, context):
     now = datetime.datetime.now()
     three_weeks_ago = now - datetime.timedelta(weeks=3)
 
-    # Member.save_from_clockify()
+    Member.save_from_clockify()
     Client.save_from_clockify()
     Project.save_from_clockify()
     TimeEntry.save_from_clockify(start=three_weeks_ago.strftime('%Y-%m-%dT%H:%M:%SZ'))
