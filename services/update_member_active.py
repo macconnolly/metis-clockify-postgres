7758d0234ac97096c1a437e0bc318f324bb2e3b5
Mac Connolly <Mac.Connolly@metisstrategy.com>
2020-09-11 07:01:51 -0600

    Update member code revised

--- services/update_member_active.py
+++ services/update_member_active.py
@@ -8,7 +8,7 @@ from datetime import datetime
 def main():
     print("Type the acronyms of the members who are no longer active.")
     print("Example: 'JNR LAB FSN PYC'")
-    acronyms = input("").lower().split()
+    acronyms = input(",").lower().split()
     for acronym in acronyms:
         member = Member.where("acronym", acronym).first()
         if member is not None:
@@ -22,9 +22,9 @@ def main():
                 date_deactivated_str, "%d/%m/%Y"
             )
             member.update()
-            print("Member with acronym {} updated".format(acronym))
+            print("Member with name {} updated".format(acronym))
         else:
-            print("Member with acronym {} not found".format(acronym))
+            print("Member with name {} not found".format(acronym))
 
 
 if __name__ == "__main__":
